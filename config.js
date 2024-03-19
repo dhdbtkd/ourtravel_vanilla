c43b979f34a29058cefcf8a46c5a34a966b1e0b8
yoosangoh <dhdbtkd@naver.com>
2024-03-19 22:15:53 +0900

    backend url - env에 따라 유동적으로 변경되도록

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const config = {
-    remoteUrl : "http://localhost:3000",
-    // remoteUrl : "https://port-0-ourtravel-backend-koh2xlivr60m6.sel4.cloudtype.app"
+    remoteUrl : import.meta.env.MODE == "development"?"http://localhost:3000":"https://port-0-ourtravel-backend-koh2xlivr60m6.sel4.cloudtype.app",
 }
 export default config;
